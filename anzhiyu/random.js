35a07379f33f91ab23cace4f608ee4310811c328
psqhhh <psqhhh@gmail.com>
2025-12-10 16:16:33 +0800

    Site updated: 2025-12-10 16:16:31

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/12/09/hello-world/"];function toRandomPost(){
+var posts=["2025/12/10/护卫光球随身携带的高维演法场 紫鸢真人传授/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
